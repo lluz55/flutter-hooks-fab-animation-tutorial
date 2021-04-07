350c172194ab85736e52730af1413922e35464c3
lluz55 <lucasluz55@gmail.com>
2021-04-07 12:47:33 -0300

    change custom hook class to function

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_hooks_fab_scroll_tutorial/hooks/scroll_controller_for_animation.dart';
 
